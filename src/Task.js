37fc451272b587585d94015bc8b09d3142a390c7
ferdiansyahwp90 <wardanaputraferdiansyah@gmail.com>
2023-06-20 10:41:18 +0700

    add

---
+++ src/Task.js
@@ -0,0 +1,5 @@
+const url = "YOURDEPLOYURL"; // TODO: replace this
+const studentName = "Ferdiansyah wardana Putra"; // TODO: replace this
+const studentId = "FE5094717"; // TODO: replace this
+
+export { url, studentName, studentId };
